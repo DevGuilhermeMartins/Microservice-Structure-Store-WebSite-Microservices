9cd61e111bd1c6fa49269b33feb656f30189de16
Guilherme Martins <guilhermemanuelmf@gmail.com>
2023-12-06 14:18:44 -0400

    Microservices Store Website

--- cambio-service/src/main/java/br/com/workcode/controller/CambioController.java
+++ cambio-service/src/main/java/br/com/workcode/controller/CambioController.java
@@ -2,6 +2,7 @@ package br.com.workcode.controller;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,6 +23,11 @@ public class CambioController {
 
 	private final CambioRepository cambioRepository;
 	
+	@GetMapping(value = "/all")
+	public List<Cambio> getAllCambio() {
+		return cambioRepository.findAll();
+	}
+	
 	@Operation(summary = "Get a value and makes a conversion by the currency from and to")
 	@GetMapping
 	public Cambio getCambio(@RequestParam("amount") BigDecimal amount,
@@ -34,7 +40,7 @@ public class CambioController {
 		if(cambio == null) new RuntimeException();
 		BigDecimal conversionFactor = cambio.getConversionFactor();
 		BigDecimal convertedValue = conversionFactor.multiply(amount);
-		convertedValue.add(tax);
+		convertedValue = convertedValue.add(tax);
 		cambio.setConvertedValue(convertedValue.setScale(2, RoundingMode.CEILING));
 		return cambio;
 	}
