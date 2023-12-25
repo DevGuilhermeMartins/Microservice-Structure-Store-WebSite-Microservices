e612f58068f80e526c4ff416f7ffed0046d6ac13
Guilherme Martins <guilhermemanuelmf@gmail.com>
2023-12-25 00:53:41 -0400

    Refactorying code

--- cambio-service/src/main/java/br/com/workcode/controller/CambioController.java
+++ cambio-service/src/main/java/br/com/workcode/controller/CambioController.java
@@ -4,8 +4,13 @@ import java.math.BigDecimal;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +19,7 @@ import br.com.workcode.dto.CambioDto;
 import br.com.workcode.model.Cambio;
 import br.com.workcode.service.CambioService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 @Tag(name = "Cambio Service API")
@@ -27,7 +33,7 @@ public class CambioController {
 	@Autowired
 	private CambioService cambioService;
 	
-	@Operation(summary = "Get a value and makes a conversion by the currency from and to")
+	@Operation(summary = "Get cambio using amount of conversion and country to convert")
 	@GetMapping
 	public ResponseEntity<CambioDto> getCambio(@RequestParam("amount") BigDecimal amount,
 							@RequestParam("to") String to) {
@@ -39,5 +45,43 @@ public class CambioController {
 		CambioDto cambioResponse = modelMapper.map(cambioEntity, CambioDto.class);
 		
 		return ResponseEntity.ok().body(cambioResponse);
+	}
+	
+	@Operation(summary = "Create Cambio")
+	@PostMapping
+	public ResponseEntity<CambioDto> saveCambio(@RequestBody CambioDto cambioDto){
+		// Convert Dto to Entity
+		Cambio cambioEntity = modelMapper.map(cambioDto, Cambio.class);
+		
+		// Make the method to create a Cambio
+		Cambio cambioCreate = cambioService.saveCambio(cambioEntity);
+		
+		// Convert Entity to Dto
+		CambioDto cambioResponse = modelMapper.map(cambioCreate, CambioDto.class);
+		
+		return new ResponseEntity<CambioDto>(cambioResponse, HttpStatus.CREATED);
+	}
+	
+	@Operation(summary = "Update Cambio")
+	@PutMapping("/{to}")
+	public ResponseEntity<CambioDto> updateCambio(@PathVariable String to, @RequestBody Cambio cambio){
+		// Find Cambio By id
+		Cambio cambioEntity = cambioService.findByTo(to);
+		
+		// Make the update Method
+		Cambio cambioUpdate = cambioService.updateCambio(to, cambioEntity);
+		
+		// Convert Entity to Dto
+		CambioDto cambioResponse = modelMapper.map(cambioUpdate, CambioDto.class);
+		
+		return ResponseEntity.ok().body(cambioResponse);
+	}
+	
+	// Delete Cambio
+	@Operation(summary = "Delete Cambio")
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> delete(@PathVariable("id") Long id) {
+		cambioService.deleteCambio(id);
+		return ResponseEntity.noContent().build();
 	}
 }
--- cambio-service/src/main/java/br/com/workcode/service/CambioService.java
+++ cambio-service/src/main/java/br/com/workcode/service/CambioService.java
@@ -3,19 +3,21 @@ package br.com.workcode.service;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.workcode.model.Cambio;
 import br.com.workcode.repository.CambioRepository;
+import lombok.AllArgsConstructor;
 
 @Service
+@AllArgsConstructor
 public class CambioService {
 
-	@Autowired
-	private CambioRepository cambioRepository;
+	
+	private final CambioRepository cambioRepository;
 	
 	
+	// Get cambio using amount of conversion and country to convert
 	public Cambio getCambio(BigDecimal amount, String to) {
 		
 		Cambio cambio = cambioRepository.findByTo(to);
@@ -24,4 +26,35 @@ public class CambioService {
 		cambio.setConvertedValue(convertedValue.setScale(2, RoundingMode.CEILING));
 		return cambio;
 	}
+	
+	// Get Cambio by country to
+	public Cambio findByTo(String to) {
+		Cambio cambioEntity = findByTo(to);
+		return cambioEntity;
+	}
+	
+	// Create Cambio
+	public Cambio saveCambio(Cambio cambio) {
+		return cambioRepository.save(cambio);
+	}
+	
+	// Update Cambio
+	public Cambio updateCambio(String to, Cambio updateCambio) {
+		Cambio cambioEntity = cambioRepository.findByTo(to);
+		updateCambio(cambioEntity, updateCambio);
+		return cambioRepository.save(cambioEntity);
+	}
+	
+	// Delete Cambio
+	public void deleteCambio(Long id) {
+		Cambio cambioEntity = cambioRepository.findById(id).orElseThrow(() -> new RuntimeException("Resource Not Found: " + id));
+		cambioRepository.delete(cambioEntity);
+	}
+
+	private void updateCambio(Cambio cambioEntity, Cambio updateCambio) {
+		cambioEntity.setTo(updateCambio.getTo());
+		cambioEntity.setConversionFactor(updateCambio.getConversionFactor());
+	}
+	
+
 }
